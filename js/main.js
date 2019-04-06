f02e4faa0e8819455eed661e4e02317edde0acc2
sioiyan <sioiyan3456@gmail.com>
2019-04-06 18:49:44 +0900

    Dateobject2

--- js/main.js
+++ js/main.js
@@ -3,19 +3,18 @@
 {
     // Date
 
-    const d = new Date();
+    // const d = new Date(2018, 11);
+    // d.setHours(10, 20, 30);
+    // d.setDate(32);
+    // d.setDate(d.getDate() + 3);
+
     // console.log(d);
 
-    // console.log(d.getFullYear());
-    // console.log(d.getMonth()); // 0 - 11
-    // console.log(d.getDate());
-    // console.log(d.getDay()); // 0 - 6
-    // console.log(d.getHours());
-    // console.log(d.getMinutes());
-    // console.log(d.getSeconds());
-    // console.log(d.getMilliseconds());
+    const d1 = new Date(2018, 11, 1);
+    const d2 = new Date(2018, 11, 10);
 
-    console.log(d.getTime()); // UTC 1970/01/01 00:00:00
+    console.log((d2 - d1) / (24 * 60 * 60 * 1000));
+
 }
 
 
