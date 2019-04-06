0fd50f79a8473b2605d01f89d68ecb6444256f37
sioiyan <sioiyan3456@gmail.com>
2019-04-06 15:48:57 +0900

    forEach

--- js/main.js
+++ js/main.js
@@ -2,14 +2,15 @@
 
 {
     // 配列
+    const a = [1, 5, 10, 100];
 
-    const a = [1, 5, 10];
-    a.splice(2, 0, 6, 7);
-    console.log(a)
+    for (let i = 0; i < a.length; i++) {
+        console.log(a[i]);
+    }
 
-    const removed = a.splice(1, 2);
-    console.log(a);
-    console.log(removed);
+    a.forEach((item, index) => {
+        console.log(`${index}:${item}`);
+    });
 }
 
 
