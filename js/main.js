50a33e27a4592ebc1cc4f241a11c75dd15071965
sioiyan <sioiyan3456@gmail.com>
2019-04-06 18:51:34 +0900

    alert,confirm

--- js/main.js
+++ js/main.js
@@ -1,19 +1,11 @@
 'use strict';
 
 {
-    // Date
+    // windows.alert('hello');
+    // alert('hello');
 
-    // const d = new Date(2018, 11);
-    // d.setHours(10, 20, 30);
-    // d.setDate(32);
-    // d.setDate(d.getDate() + 3);
-
-    // console.log(d);
-
-    const d1 = new Date(2018, 11, 1);
-    const d2 = new Date(2018, 11, 10);
-
-    console.log((d2 - d1) / (24 * 60 * 60 * 1000));
+    const answer = confirm('Are you sure?');
+    console.log(answer);
 
 }
 
