19d91d09e61fd0b868326089df2a51720f88a68a
sioiyan <sioiyan3456@gmail.com>
2019-04-06 15:13:39 +0900

    array_element

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@
 {
     // 配列
 
-    // const score1 = 80;    
-    // const score2 = 90;    
-    // const score3 = 40;
-    
     const scores = [80, 90, 40];
-    console.log(scores)
+    // console.log(scores[1]); // 90
+
+    // scores[2] = 100;
+    // console.log(scores);
+
+    console.log(scores.length); // 3
 }
