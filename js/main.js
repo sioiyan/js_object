c05e6038839dda64cd905c82cabb9a9e0a1b3ed8
sioiyan <sioiyan3456@gmail.com>
2019-04-06 15:45:10 +0900

    splice

--- js/main.js
+++ js/main.js
@@ -4,19 +4,12 @@
     // 配列
 
     const a = [1, 5, 10];
+    a.splice(2, 0, 6, 7);
+    console.log(a)
 
-
-    // add
-    // a.unshift(100);
-    // a.push(200, 300);
-
-    // console.log(a);
-
-    // remove
-    a.shift();
-    a.pop();
-
+    const removed = a.splice(1, 2);
     console.log(a);
+    console.log(removed);
 }
 
 
