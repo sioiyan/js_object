06ed6947d9424d63efddebe61371d2ae660437ba
sioiyan <sioiyan3456@gmail.com>
2019-04-06 15:19:11 +0900

    operate object

--- js/main.js
+++ js/main.js
@@ -9,5 +9,15 @@
         score: 32,
     };
 
+    // console.log(player.name);
+    // console.log(player['name']);
+
+    // player.score = 100;
+    // console.log(player);
+
+    // player.email = 'taguchi@gmail.com';
+    // console.log(player);
+
+    delete player.score;
     console.log(player);
 }
