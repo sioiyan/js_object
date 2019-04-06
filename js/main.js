4c488a52f614f0febece16eaf8293d63326d19a7
sioiyan <sioiyan3456@gmail.com>
2019-04-06 15:27:23 +0900

    use method

--- js/main.js
+++ js/main.js
@@ -1,22 +1,29 @@
 'use strict';
 
 {
-    // オブジェクト
-
-    // const player = {
-    //     name: taguchi,
-    //     score: 32,
-    // };
-    // const fkoji = {
-    //     name: fkoji,
-    //     score: 44,
-    // };
 
     class Player {
         constructor(name, score) { //メソッド
             this.name = name;
             this.score = score;
         }
+
+        showInfo() {
+            console.log(`name: ${this.name} score: ${this.score}`);
+        }
+
+        static showVersion() {
+            console.log('Player class ver. 1.0');
+        }
+
     }
     
+    const taguchi = new Player('taguchi', 32);
+    const fkoji = new Player('fkoji', 44);
+
+    // console.log(taguchi.name);
+    fkoji.showInfo();
+    taguchi.showInfo();
+
+    Player.showVersion();
 }
