21f856eff45197026dd528973ee33217e37d0730
sioiyan <sioiyan3456@gmail.com>
2019-04-06 16:08:34 +0900

    use spread-operator

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 'use strict';
 
 {
-    // Object
 
-    const o = {
-        a: 1,
-        b: 2,
-    };
+    // const a = [10, 20];
+    // const b = [1, 2, ...a];
+    // console.log(b);
+    // a[0] = 100;
+    // console.log(b);
 
-    // console.log(Object.keys(o)); // ['a', 'b']
-    // console.log(Object.values(o)); // [1, 2]
-    // console.log(Object.entries(o)); // [['a', 1],['b', 2]]
-    
-    Object.keys(o).forEach(key => {
-        console.log(`${key}: ${o[key]}`);
-    });
+    // const a = [10, 20];
+    // const sum = (a, b) => a + b;
+    // console.log(sum(...a));
+
+    const o1 = {a: 1};
+    const o2 = {...o1, b: 2};
+    console.log(o2);
 }
 
 
