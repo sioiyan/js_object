24d48703551790a306d575387ebc2cf42d48fa05
sioiyan <sioiyan3456@gmail.com>
2019-04-06 15:54:27 +0900

    map,filter

--- js/main.js
+++ js/main.js
@@ -4,13 +4,17 @@
     // 配列
     const a = [1, 5, 10, 100];
 
-    for (let i = 0; i < a.length; i++) {
-        console.log(a[i]);
-    }
+    // const b = [];
+    // a.forEach(item => {
+    //     b.push(item * 2);
+    // });
+    // console.log(b);
 
-    a.forEach((item, index) => {
-        console.log(`${index}:${item}`);
-    });
+    // const b = a.map(item => item * 2);
+    // console.log(b);
+
+    const b = a.filter(item => item % 2 === 0);
+    console.log(b);
 }
 
 
