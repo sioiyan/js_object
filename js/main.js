efb34581aeeae22ecaccffd821d1bda6b9bfa987
sioiyan <sioiyan3456@gmail.com>
2019-04-06 16:26:41 +0900

    Mathobject

--- js/main.js
+++ js/main.js
@@ -1,26 +1,13 @@
 'use strict';
 
 {
-    // const numbers = [1, 2, 3, 8];
-    // const a = numbers[0];
-    // const b = numbers[1];
-    // const [a, b, ...rest] = numbers;
-    // console.log(a);
-    // console.log(b);
-    // console.log(rest);
- 
-    const player = {
-        name: 'taguchi',
-        score: 55,
-        hp: 33,
-        mp: 22,
-    };
+    // Math
 
-    const {name, score, ...points} = player;
-    console.log(name);
-    console.log(score);
-    console.log(points);
+    // console.log(Math.PI);
 
+    // console.log(Math.random());
+
+    console.log(Math.floor(Math.random() * 6 + 1));
 }
 
 
